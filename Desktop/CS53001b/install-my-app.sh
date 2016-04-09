04f938808bc88951b4550772f6c96f16deda810e
Runze Si <sir@dhcp-hol-1184.redrover.cornell.edu>
2016-04-09 13:55:11 -0400

    Another minor update

--- Desktop/CS53001b/install-my-app.sh
+++ Desktop/CS53001b/install-my-app.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-S3_BUCKET=mys3bucket
+S3_BUCKET=edu-cornell-cs-cs5300s16-rs2357-proj1b
 
 echo "your install instructions for a single node go here"
 echo "you could do this: aws s3 cp s3://${S3_BUCKET}/hello.war /path/to/tomcat/webapps/hello.war"
@@ -8,6 +7,7 @@ echo "you would write to simpledb here"
 echo "you would loop reading simpledb here once every X seconds (say, 15?) until all nodes online"
 
 yum -y install tomcat8-webapps tomcat8-docs-webapp tomcat8-admin-webapps #Install the Tomcat server
+aws s3 cp s3://${S3_BUCKET}/hello.war /path/to/tomcat/webapps/hello.war #Download the war file to each instance
 <host>:8080/hello #Need to install the application code here
 #Any other software?
 wget http://169.254.169.254/latest/meta-data/local-ipv4 #3.1.2 Create a file names local-ipv4 that contains the internal ip address of the instance

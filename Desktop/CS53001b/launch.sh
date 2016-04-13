d28043ce4b71f3ed9cfa06c70ae9cb2fa3b89f1d
Runze Si <sir@dhcp-hol-3072.redrover.cornell.edu>
2016-04-12 20:27:50 -0400

    Updated with launch script

--- Desktop/CS53001b/launch.sh
+++ Desktop/CS53001b/launch.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-AMI=amzn-ami-hvm-2016.03.0.x86_64-gp2 (ami-c229c0a2) # EC2 Instances Control Panel, AMI ID
-S3_BUCKET=cs5300-bjg226-project1b # S3 Bucket
+accessKey
+secretAccessKey
+AMI=ami-97a7b4fd # EC2 Instances Control Panel, AMI ID
+S3_BUCKET=edu-cornell-cs-cs5300s16-rs2357-proj1b # S3 Bucket
 
-aws configure set aws_access_key_id AKIAJYTRJUGW4XRMLOLQ # Account -> Security Credentials ->Users -> brandongiraldo -> Security Credentials
-aws configure set aws_secret_access_key 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 # Account -> Security Credentials ->Users -> brandongiraldo -> Security Credentials
-aws configure set default.region us-east-1 # Leave this as is
+#aws configure set aws_access_key_id AKIAJYTRJUGW4XRMLOLQ # Account -> Security Credentials ->Users -> brandongiraldo -> Security Credentials
+#aws configure set aws_secret_access_key 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 # Account -> Security Credentials ->Users -> brandongiraldo -> Security Credentials
+#aws configure set default.region us-east-1 # Leave this as is
+
+aws configure set aws_access_key_id AKIAICNPLUSMFTAXOLTQ
+aws configure set aws_secret_access_key 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
+aws configure set default.region us-east-1
 
 aws s3 cp hello.war s3://${S3_BUCKET}/hello.war # 
 aws ec2 run-instances --image-id ${AMI} --count 5 --instance-type t2.micro --user-data install-my-app.sh

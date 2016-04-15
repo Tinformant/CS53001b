beb7c5edc716127769804fa51c7283301698d48c
brandongiraldo <brandongiraldo10@hotmail.com>
2016-04-14 22:07:36 -0400

    fixing install-my-app.sh

--- CS53001b/launch.sh
+++ CS53001b/launch.sh
@@ -1,12 +1,9 @@
-#!/bin/bash
-AMI=XXX
-S3_BUCKET=mys3bucket
+AMI=ami-8f1409e5 # EC2 Instances Control Panel, AMI ID
+S3_BUCKET=edu-cornell-cs-cs5300s16-rs2357-proj1b # S3 Bucket
 
-aws configure set aws_access_key_id XXXXXXXXXXXXXXXXX 
-aws configure set aws_secret_access_key YYYYYYYYYYYYYYYYYYY 
+aws configure set aws_access_key_id AKIAIHZOADZ35ISWMXZA
+aws configure set aws_secret_access_key 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
 aws configure set default.region us-east-1
 
-aws s3 cp hello.war s3://${S3_BUCKET}/hello.war
+# aws s3 cp hello.war s3://${S3_BUCKET}/hello.war # war can manually be put on S3
 aws ec2 run-instances --image-id ${AMI} --count 5 --instance-type t2.micro --user-data install-my-app.sh
-
-

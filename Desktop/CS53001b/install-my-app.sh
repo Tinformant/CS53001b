f429181952a4444040dc1b86b24192e70308eabc
Tinformant <rs2357@cornell.edu>
2016-04-17 16:06:29 -0400

    Added files via upload

--- Desktop/CS53001b/install-my-app.sh
+++ Desktop/CS53001b/install-my-app.sh
@@ -6,15 +6,24 @@ echo "you could do this: aws s3 cp s3://${S3_BUCKET}/hello.war /usr/share/tomcat
 echo "you would write to simpledb here"
 echo "you would loop reading simpledb here once every X seconds (say, 15?) until all nodes online"
 
-yum -y install tomcat8-webapps tomcat8-docs-webapp tomcat8-admin-webapps #Install the Tomcat server
-aws s3 cp s3://${S3_BUCKET}/hello.war /path/to/tomcat/webapps/hello.war #Download the war file to each instance
-<host>:8080/hello #Need to install the application code here
+sudo yum update
+sudo yum install tomcat8
+sudo yum -y install tomcat8-webapps tomcat8-docs-webapp tomcat8-admin-webapps #Install the Tomcat server
+#Have to enable security group
+#aws s3 cp s3://${S3_BUCKET}/hello.war /usr/share/tomcat8/webapps/hello.war #Download the war file to each instance
+aws s3 cp s3://edu-cornell-cs-cs5300s16-rs2357-proj1b/test-app.war /usr/share/tomcat8/webapps/test-app.war
+sudo chmod o+w /usr
+sudo chmod o+w /usr/share
+sudo chmod o+w /usr/share/tomcat8
+sudo chown ec2-user:ec2-user /usr/share/tomcat8/webapps
+aws s3 cp s3://edu-cornell-cs-cs5300s16-rs2357-proj1b/test-app.war /usr/share/tomcat8/webapps/test-app.war
+#<host>:8080/hello #Need to install the application code here
 #Any other software?
-wget http://169.254.169.254/latest/meta-data/local-ipv4 #3.1.2 Create a file names local-ipv4 that contains the internal ip address of the instance
-ami=$(wget http://169.254.169.254/latest/meta-data/ami-launch-index) #3.1.3 Determine the instance's ami-launch-index
-#Upload the internal IP address to SimpleDB at a domain + item + attribute name
+amiLaunchIndex=$(curl http://169.254.169.254/latest/meta-data/ami-launch-index)
+internalIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+publicHostName=$(curl http://169.254.169.254/latest/meta-data/hostname) #Return public DNS
+aws sdb put-attributes --domain-name serverList --item-name $amiLaunchIndex --attributes Name="Internal IP",Value=$internalIP Name="Public Host Name", Value=$publicHostName
 
-aws configure set preview.sdb true #Enable SimpleDB
+#aws configure set preview.sdb true #Enable SimpleDB
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #Find the current directory
-sh $DIR/startup #3.1.6 Start the tomcat server
+sudo service tomcat8 start
--- Desktop/CS53001b/launch.sh
+++ Desktop/CS53001b/launch.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-AMI=ami-97a7b4fd # EC2 Instances Control Panel, AMI ID
+AMI=ami-08111162 # EC2 Instances Control Panel, AMI ID
 S3_BUCKET=edu-cornell-cs-cs5300s16-rs2357-proj1b # S3 Bucket
 
 #aws configure set aws_access_key_id AKIAJYTRJUGW4XRMLOLQ # Account -> Security Credentials ->Users -> brandongiraldo -> Security Credentials
 #aws configure set aws_secret_access_key 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 # Account -> Security Credentials ->Users -> brandongiraldo -> Security Credentials
 #aws configure set default.region us-east-1 # Leave this as is
 
-aws configure set aws_access_key_id AKIAICNPLUSMFTAXOLTQ
-aws configure set aws_secret_access_key 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
+aws configure set aws_access_key_id AKIAIUFZXMYYC7ZJJH7A
+aws configure set aws_secret_access_key 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
 aws configure set default.region us-east-1
 
-aws s3 cp hello.war s3://${S3_BUCKET}/hello.war # 
-aws ec2 run-instances --image-id ${AMI} --count 5 --instance-type t2.micro --user-data install-my-app.sh
+aws configure set preview.sdb true
+aws sdb create-domain --domain-name serverList1
+
+#aws ec2 create-security-group --group-name mySecurityGroup --description "My security group"
+#aws ec2 authorize-security-group-ingress --group-name mySecurityGroup --protocol tcp --port 22 --cidr 0.0.0.0/0
+#aws ec2 authorize-security-group-ingress --group-name mySecurityGroup --protocol tcp --port 8080 --cidr 0.0.0.0/0
+
+#aws s3 cp hello.war s3://${S3_BUCKET}/hello.war 
+#Problem with ami, is a key-pair required?
+aws ec2 run-instances --image-id ${AMI} --count 1 --instance-type t2.micro --key-name SiR --user-data install-my-app.sh --security-groups mySecurityGroup
 
 

c30911ba3fe8a7e595d41609c39d06f78f932fa7
brandongiraldo <brandongiraldo10@hotmail.com>
2016-04-17 17:55:18 -0400

    New install-my-app.sh file

--- Desktop/CS53001b/install-my-app.sh
+++ Desktop/CS53001b/install-my-app.sh
@@ -6,12 +6,12 @@ echo "you could do this: aws s3 cp s3://${S3_BUCKET}/hello.war /usr/share/tomcat
 echo "you would write to simpledb here"
 echo "you would loop reading simpledb here once every X seconds (say, 15?) until all nodes online"
 
-sudo yum update
-sudo yum install tomcat8
+sudo yum -y update
+sudo yum -y install tomcat8
 sudo yum -y install tomcat8-webapps tomcat8-docs-webapp tomcat8-admin-webapps #Install the Tomcat server
 #Have to enable security group
 #aws s3 cp s3://${S3_BUCKET}/hello.war /usr/share/tomcat8/webapps/hello.war #Download the war file to each instance
-aws s3 cp s3://edu-cornell-cs-cs5300s16-rs2357-proj1b/test-app.war /usr/share/tomcat8/webapps/test-app.war
+# aws s3 cp s3://edu-cornell-cs-cs5300s16-rs2357-proj1b/test-app.war /usr/share/tomcat8/webapps/test-app.war
 sudo chmod o+w /usr
 sudo chmod o+w /usr/share
 sudo chmod o+w /usr/share/tomcat8
